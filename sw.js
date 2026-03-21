3bac1bba95d93f7c39e720d78b0cb0a720a6f75f
oiurioficial <oiurioficial@gmail.com>
2026-03-21 17:38:33 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,25 @@
-self.addEventListener('install', (event) => {
+const CACHE_NAME = "raiz-v1";
+
+const urlsToCache = [
+  "/",
+  "/p/plataforma-raiz-v2.html"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
+self.addEventListener("activate", event => {
   event.waitUntil(clients.claim());
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
